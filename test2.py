93c3ea200f5d658b7b5cb790515bb6ea1ddb1ea9
aditya <69519212+Stepin104341@users.noreply.github.com>
2020-11-10 09:41:03 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,101 +1,74 @@
-""" Tic Tac Toe
-----------------------------------------
-"""
-import random
-import sys
-board=[i for i in range(0,9)]
-player, computer = '',''
-#Corners, Center and Others, respectively
-moves=((1,7,3,9),(5,),(2,4,6,8))
-#Winner combinations
-winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-# Table
-tab=range(1,10)
-def print_board():
-    x=1
-    for i in board:
-        end = ' | '
-        if x%3 == 0:
-            end = ' \n'
-            if i != 1: end+='---------\n';
-        char=' '
-        if i in ('X','O'): char=i;
-        x+=1
-        print(char,end=end)
-def select_char():
-    chars=('X','O')
-    if random.randint(0,1) == 0:
-        return chars[::-1]
-    return chars
-def can_move(brd, player, move):
-    if move in tab and brd[move-1] == move-1:
-        return True
-    return False
-def can_win(brd, player, move):
-    places=[]
-    x=0
-    for i in brd:
-        if i == player: places.append(x);
-        x+=1
-    win=True
-    for tup in winners:
-        win=True
-        for ix in tup:
-            if brd[ix] != player:
-                win=False
-                break
-        if win == True:
-            break
-    return win
-def make_move(brd, player, move, undo=False):
-    if can_move(brd, player, move):
-        brd[move-1] = player
-        win=can_win(brd, player, move)
-        if undo:
-            brd[move-1] = move-1
-        return (True, win)
-    return (False, False)
-#AI goes here
-def computer_move():
-    move=-1
-    # If I can win, others do not matter.
-    for i in range(1,10):
-        if make_move(board, computer, i, True)[1]:
-            move=i
-            break
-    if move == -1:
-       # If player can win, block him.
-        for i in range(1,10):
-            if make_move(board, player, i, True)[1]:
-                move=i
-                break
-    if move == -1:
-        # Otherwise, try to take one of desired places.
-        for tup in moves:
-            for mv in tup:
-                if move == -1 and can_move(board, computer, mv):
-                    move=mv
-                    break
-    return make_move(board, computer, move)
-def space_exist():
-    return board.count('X') + board.count('O') != 9
-player, computer = select_char()
-print('Player is [%s] and computer is [%s]' % (player, computer))
-result='%%% Deuce ! %%%'
-while space_exist():
-    print_board()
-    print('#Make your move ! [1-9] : ', end='')
-    move = int(input())
-    moved, won = make_move(board, player, move)
-    if not moved:
-        print(' >> Invalid number ! Try again !')
-        continue
-    
-    if won:
-        result='*** Congratulations ! You won ! ***'
-        break
-    elif computer_move()[1]:
-        result='=== You lose ! =='
-        break;
-print_board()
-print(result)
+TOTAL_DISK = 3
+
+
+def main(): 
+    print('started.')
+
+    toh = TowerOfHanoi(TOTAL_DISK)
+    toh.run()
+
+    print('finished.')
+
+
+class Tower: 
+    """Represents a tower."""
+
+    def __init__(self, name, n_disks = 0): 
+        self.name = name
+        self.disks = []
+
+        for i in range(n_disks, 0, -1): 
+            self.push(str(i))
+
+    def push(self, disk): 
+        self.disks.append(disk)
+
+    def pop(self): 
+        return self.disks.pop()
+
+    def __str__(self): 
+        disks = ''.join('{:<2}'.format(d) for d in self.disks)
+        return '{}: {}'.format(self.name, disks)
+
+
+class TowerOfHanoi:  
+    """Represents the Tower of Hanoi's problem and solution."""
+
+    def __init__(self, n_disk): 
+        self.n_disk = n_disk
+        self.n_move = 0
+        self.prepare_towers()
+
+    def prepare_towers(self): 
+        s = Tower('s', self.n_disk)
+        d = Tower('d')
+        e = Tower('e')
+
+        self.towers = [s, d, e]
+
+    def run(self): 
+        self.show_towers()
+        s, d, e = self.towers
+        self.move_disk(s, d, e, self.n_disk)
+
+    def move_disk(self, s, d, e, n): 
+        if n <= 0: 
+            return
+
+        self.move_disk(s, e, d, n - 1)
+
+        self.n_move += 1
+        print('move disk-{} from {} to {} ({}).'.format(str(n), s.name, d.name, self.n_move))
+        disk = s.pop()
+        d.push(disk)
+        self.show_towers()
+
+        self.move_disk(e, d, s, n - 1)
+
+    def show_towers(self): 
+        for t in self.towers: 
+            print(t)
+
+
+if __name__ == '__main__': 
+    main()
