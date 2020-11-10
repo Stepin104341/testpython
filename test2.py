f155e2bddb1a07c9c91dd25d7c735eb928a2059c
aditya <69519212+Stepin104341@users.noreply.github.com>
2020-11-10 09:19:41 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,104 +1,101 @@
-from tkinter import *
-from tkinter import messagebox
-
-calculator = Tk()
-calculator.title("CALCULATOR")
-calculator.resizable(0, 1)#remove or change this in order to get different screen sizes
-
-class Application(Frame):
-	def __init__(self, master, *args, **kwargs):
-		Frame.__init__(self, master, *args, **kwargs)
-		self.createWidgets()
-
-	def replaceText(self, text):
-		self.display.delete(0, END)
-		self.display.insert(0, text)
-
-	def appendToDisplay(self, text):
-		self.entryText = self.display.get()
-		self.textLength = len(self.entryText)
-
-		if self.entryText == "0":
-			self.replaceText(text)
-		else:
-			self.display.insert(self.textLength, text)
-
-	def calculateExpression(self):#python's calculate function 
-		self.expression = self.display.get()
-		self.expression = self.expression.replace("%", "/ 100")
-
-		try:
-			self.result = eval(self.expression)
-			self.replaceText(self.result)
-		except:
-			messagebox.showinfo("ERROR", "Invalid input", icon="warning", parent=calculator)
-
-	def clearText(self):#clears imput on pressing C on Calculator
-		self.replaceText("0")
-
-	def createWidgets(self):
-		self.display = Entry(self, font=("Helvetica", 16), borderwidth=0, relief=RAISED, justify=RIGHT)
-		self.display.insert(0, "0")
-		self.display.grid(row=0, column=0, columnspan=5)
-
-#This is the First Row
-		self.sevenButton = Button(self, font=("Helvetica", 11), text="7", borderwidth=0, command=lambda: self.appendToDisplay("7"))
-		self.sevenButton.grid(row=1, column=0, sticky="NWNESWSE")
-
-		self.eightButton = Button(self, font=("Helvetica", 11), text="8", borderwidth=0, command=lambda: self.appendToDisplay("8"))
-		self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
-
-		self.nineButton = Button(self, font=("Helvetica", 11), text="9", borderwidth=0, command=lambda: self.appendToDisplay("9"))
-		self.nineButton.grid(row=1, column=2, sticky="NWNESWSE")
-
-		self.timesButton = Button(self, font=("Helvetica", 11), text="*", borderwidth=0, command=lambda: self.appendToDisplay("*"))
-		self.timesButton.grid(row=1, column=3, sticky="NWNESWSE")
-
-		self.clearButton = Button(self, font=("Helvetica", 11), text="C", borderwidth=0, command=lambda: self.clearText())
-		self.clearButton.grid(row=1, column=4, sticky="NWNESWSE")
-
-#This is the Second Row
-		self.fourButton = Button(self, font=("Helvetica", 11), text="4", borderwidth=0, command=lambda: self.appendToDisplay("4"))
-		self.fourButton.grid(row=2, column=0, sticky="NWNESWSE")
-
-		self.fiveButton = Button(self, font=("Helvetica", 11), text="5", borderwidth=0, command=lambda: self.appendToDisplay("5"))
-		self.fiveButton.grid(row=2, column=1, sticky="NWNESWSE")
-
-		self.sixButton = Button(self, font=("Helvetica", 11), text="6", borderwidth=0, command=lambda: self.appendToDisplay("6"))
-		self.sixButton.grid(row=2, column=2, sticky="NWNESWSE")
-
-		self.divideButton = Button(self, font=("Helvetica", 11), text="/", borderwidth=0, command=lambda: self.appendToDisplay("/"))
-		self.divideButton.grid(row=2, column=3, sticky="NWNESWSE")
-
-		self.percentageButton = Button(self, font=("Helvetica", 11), text="%", borderwidth=0, command=lambda: self.appendToDisplay("%"))
-		self.percentageButton.grid(row=2, column=4, sticky="NWNESWSE")
-
-#This is the Third Row
-		self.oneButton = Button(self, font=("Helvetica", 11), text="1", borderwidth=0, command=lambda: self.appendToDisplay("1"))
-		self.oneButton.grid(row=3, column=0, sticky="NWNESWSE")
-
-		self.twoButton = Button(self, font=("Helvetica", 11), text="2", borderwidth=0, command=lambda: self.appendToDisplay("2"))
-		self.twoButton.grid(row=3, column=1, sticky="NWNESWSE")
-
-		self.threeButton = Button(self, font=("Helvetica", 11), text="3", borderwidth=0, command=lambda: self.appendToDisplay("3"))
-		self.threeButton.grid(row=3, column=2, sticky="NWNESWSE")
-
-		self.minusButton = Button(self, font=("Helvetica", 11), text="-", borderwidth=0, command=lambda: self.appendToDisplay("-"))
-		self.minusButton.grid(row=3, column=3, sticky="NWNESWSE")
-
-		self.equalsButton = Button(self, font=("Helvetica", 11), text="=", borderwidth=0, command=lambda: self.calculateExpression())
-		self.equalsButton.grid(row=3, column=4, sticky="NWNESWSE", rowspan=2)
-
-#This is the Fourth Row
-		self.zeroButton = Button(self, font=("Helvetica", 11), text="0", borderwidth=0, command=lambda: self.appendToDisplay("0"))
-		self.zeroButton.grid(row=4, column=0, columnspan=2, sticky="NWNESWSE")
-
-		self.dotButton = Button(self, font=("Helvetica", 11), text=".", borderwidth=0, command=lambda: self.appendToDisplay("."))
-		self.dotButton.grid(row=4, column=2, sticky="NWNESWSE")
-
-		self.plusButton = Button(self, font=("Helvetica", 11), text="+", borderwidth=0, command=lambda: self.appendToDisplay("+"))
-		self.plusButton.grid(row=4, column=3, sticky="NWNESWSE")
-
-
-app = Application(calculator).grid()		
-calculator.mainloop()
+""" Tic Tac Toe
+----------------------------------------
+"""
+import random
+import sys
+board=[i for i in range(0,9)]
+player, computer = '',''
+#Corners, Center and Others, respectively
+moves=((1,7,3,9),(5,),(2,4,6,8))
+#Winner combinations
+winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+# Table
+tab=range(1,10)
+def print_board():
+    x=1
+    for i in board:
+        end = ' | '
+        if x%3 == 0:
+            end = ' \n'
+            if i != 1: end+='---------\n';
+        char=' '
+        if i in ('X','O'): char=i;
+        x+=1
+        print(char,end=end)
+def select_char():
+    chars=('X','O')
+    if random.randint(0,1) == 0:
+        return chars[::-1]
+    return chars
+def can_move(brd, player, move):
+    if move in tab and brd[move-1] == move-1:
+        return True
+    return False
+def can_win(brd, player, move):
+    places=[]
+    x=0
+    for i in brd:
+        if i == player: places.append(x);
+        x+=1
+    win=True
+    for tup in winners:
+        win=True
+        for ix in tup:
+            if brd[ix] != player:
+                win=False
+                break
+        if win == True:
+            break
+    return win
+def make_move(brd, player, move, undo=False):
+    if can_move(brd, player, move):
+        brd[move-1] = player
+        win=can_win(brd, player, move)
+        if undo:
+            brd[move-1] = move-1
+        return (True, win)
+    return (False, False)
+#AI goes here
+def computer_move():
+    move=-1
+    # If I can win, others do not matter.
+    for i in range(1,10):
+        if make_move(board, computer, i, True)[1]:
+            move=i
+            break
+    if move == -1:
+       # If player can win, block him.
+        for i in range(1,10):
+            if make_move(board, player, i, True)[1]:
+                move=i
+                break
+    if move == -1:
+        # Otherwise, try to take one of desired places.
+        for tup in moves:
+            for mv in tup:
+                if move == -1 and can_move(board, computer, mv):
+                    move=mv
+                    break
+    return make_move(board, computer, move)
+def space_exist():
+    return board.count('X') + board.count('O') != 9
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
+result='%%% Deuce ! %%%'
+while space_exist():
+    print_board()
+    print('#Make your move ! [1-9] : ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print(' >> Invalid number ! Try again !')
+        continue
+    
+    if won:
+        result='*** Congratulations ! You won ! ***'
+        break
+    elif computer_move()[1]:
+        result='=== You lose ! =='
+        break;
+print_board()
+print(result)
